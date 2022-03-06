f3c017fcd9bcb992a7ef33ab8bd4720f17715498
Henrifdez <paulo.fernandes3032@gmail.com>
2022-03-06 18:27:32 -0300

    python

--- teste.py
+++
@@ -1,9 +0,0 @@
-class computador:
-    def _int_(self,marca,madelo):
-        self.marca = ''
-        self.modelo = ''
-    def ligar(self):
-        print(f"ligando o {self.marca} modelo {self.modelo}")
-
-computador_1 = computador('Dell', 'xps')
-computador_1.ligar()
